5c72c91075c5c2d6b372b11457324f1650cbc66d
Abdijabar Yussuf <87280648+Abdijabaryussuf@users.noreply.github.com>
2026-09-23 10:53:09 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Shimbir",
+  description: "Learn. Master. Fly higher.",
+};
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
